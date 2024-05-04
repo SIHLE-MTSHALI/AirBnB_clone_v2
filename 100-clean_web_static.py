ccc111d7842d8b0ca70162d9e9b89dabb7a09790
Sihle <sihle.mtshali8@gmail.com>
2024-05-03 18:38:58 -0700

    Update clean script to correctly handle archive removal

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Fabric script to delete out-of-date archives
+Fabric script that deletes out-of-date archives both locally and on web servers
 """
 from fabric.api import *
 
-env.hosts = ['<IP web-01>', '<IP web-02>']
+env.hosts = ['18.210.13.239', '18.214.89.149']
 
 def do_clean(number=0):
-    """Cleans up old archives."""
+    """ Cleans up old archives. """
     number = int(number) if int(number) > 0 else 1
-    local(f"ls -tr versions/*.tgz | head -n -{number} | xargs rm -rf")
-    run(f"ls -tr /data/web_static/releases/ | head -n -{number} | \
+    local(f"ls -t versions/*.tgz | tail -n +{number + 1} | xargs rm -f")
+    run(f"ls -t /data/web_static/releases/ | tail -n +{number + 1} | \
          xargs -I {{}} rm -rf /data/web_static/releases/{{}}")
