536d42e73531f075a694a55353baf98f1c79d454
Sihle <sihle.mtshali8@gmail.com>
2024-05-03 17:47:08 -0700

    Task 4: Clean old archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,25 +2,13 @@
 """
 Fabric script to delete out-of-date archives
 """
-from fabric.api import env, local, run
+from fabric.api import *
 
 env.hosts = ['<IP web-01>', '<IP web-02>']
 
-
 def do_clean(number=0):
-    """Deletes out-of-date archives"""
-    number = int(number)
-    if number < 0:
-        return False
-
-    number = 1 if number == 0 else number + 1
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    """Cleans up old archives."""
+    number = int(number) if int(number) > 0 else 1
+    local(f"ls -tr versions/*.tgz | head -n -{number} | xargs rm -rf")
+    run(f"ls -tr /data/web_static/releases/ | head -n -{number} | \
+         xargs -I {{}} rm -rf /data/web_static/releases/{{}}")
